01b96a835466db724535f557942427cc36cbf1d3
GaneshNagpure <128400584+GaneshNagpure@users.noreply.github.com>
2023-05-31 09:06:13 +0530

    Add files via upload

---
+++ ActionSendKey.java
@@ -0,0 +1,25 @@
+package Actions;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionSendKey {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demo.actitime.com/login.do");
+        WebElement button = driver.findElement(By.id("loginButton"));
+		Actions a = new Actions(driver);
+		a.sendKeys(button, Keys.ENTER);
+	}
+
+}
